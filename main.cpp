1e47577b3468ad5d2afbafdb5bb7a19dd948cdd1
danis <danissimo270902@gmail.com>
2021-06-15 10:06:48 +0300

    Unit test try

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 #include <iostream>
+
 
 std::pair<int, std::pair<char, char>> mostAscendingSequence(std::string sequence){
 
@@ -33,8 +36,38 @@ std::pair<int, std::pair<char, char>> mostAscendingSequence(std::string sequence
 
     return res;
 }
-
-int main() {
+void test(){
     std::pair<int, std::pair<char, char>> res = mostAscendingSequence("abasdafdfsdfdv");
-    std::cout<< res.first << "\t" << res.second.first << "\t" << res.second.second;
+            CHECK(5, res.first);
+            CHECK('d', res.second.first);
+            CHECK('s', res.second.second);
+}
+TEST_CASE("cheaking funcion for corection") {
+    test();
+}
+
+int main(int argc, char** argv) {
+    doctest::Context context;
+
+
+
+    // defaults
+//    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
+//    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
+//    context.setOption("order-by", "name");            // sort the test cases by their name
+
+    context.applyCommandLine(argc, argv);
+
+    // overrides
+//    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+
+    int res = context.run(); // run
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return res;          // propagate the result of the tests
+
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
+
+    return res + client_stuff_return_code; // the result from doctest is propagated here as well
 }
